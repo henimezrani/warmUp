cc0d158607d3009f7194cd8e56678e98c4f5c490
henimezrani <henimezrani@gmail.com>
2019-10-24 09:40:01 +0100

    updating warmUp 7

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,48 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+// A - 1
+
+
+var firstName = "Mohamed";
+var lastName = "Slamia";
+
+var fullName = firstName + " " + lastName;
+
+fullname; // => "Mohamed Slamia"
+
+
+// A - 2
+
+
+function is13MultipleOf3() {
+	return 13 % 3 === 0 ? "13 is multiple of 3" : "13 is NOT multiple of 3"
+}
+
+is13MultipleOf3() // => 13 is NOT multiple of 3
+
+
+// A - 3
+
+
+function averageAge(arr) {
+	var total = 0;
+	for (var i = 0 ; i < arr.length ; i++) {
+		total += arr[i];
+	}
+	return total / arr.length;
+}
+
+averageAge([13,14,13,15,16,17,19,13,16,15]) // => 15.1
+
+
+// A - 4 
+
+
+function calcAgeInSeconds(age) {
+	return age * 365 * 24 * 60 * 60;
+}
+
+calcAgeInSeconds(23); // => 725328000
