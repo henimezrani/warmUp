8d4b4cd93fcaf123de2b90169637529672804669
henimezrani <henimezrani@gmail.com>
2019-10-31 10:29:31 +0100

    warmUp 12

--- warmUp12.js
+++ warmUp12.js
@@ -22,3 +22,83 @@ Technical Details
     -The maximum size of a range will be 100 integers
     -The starting number of a range will be: 0 < n < 100
  */
+
+function mostRepeted(str){
+    var mostRepetedChar = ""
+    var maxCount = 0;
+    for (var i = 0; i < str.length; i++) {
+        count = 0
+        for (var j = 0; j < str.length; j++) {
+            if (str[i] = str[j]) {
+                count++
+            }
+        }
+        if (count>maxCount){
+            mostRepetedChar = str[i];
+            maxCount = count;
+        }
+    }
+    return mostRepetedChar;
+}
+
+function detectMin (array) {
+    var min = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (min > array[i]) {
+            min = array[i];
+        }
+    }
+    return min
+}
+
+function smallestLargest (str, size) {
+    if (str.length> size*2){ 
+
+        // Meaning that there will be the number 100 in the range
+        // We start by 100, go up until we dont find a number n
+        // n-1 will be the max and n-range the min
+
+        var mostRepetedCharacter ="10"
+        var otherCount =0;
+        var tmpNbr;
+        while(true){
+            tmpNbr = mostRepetedCharacter + otherCount.toString()
+            if (str.indexOf(tmpNbr) < 0){
+                return [tmpNbr-10, tmpNbr-1]
+                break;
+            }
+            otherCount++
+            if (otherCount === 10) {
+                mostRepetedCharacter = (mostRepetedCharacter.parseInt()+1).toString()
+                otherCount = 0
+            }
+        }
+    }
+    if ((str.length === size) && (size <= 10)){
+
+        // Meaning that the range is small and the numbers in the string are between 0 and 9
+        //Just detect the min and the max
+
+        var arr = str.split('');
+        return [detectMin(arr), detectMin(arr)+size-1]
+    }
+
+
+    // This is where all the numbers are either 1 or 2 digit long
+    
+    var mostRepetedCharacter = mostRepeted(str);
+    var otherCount =0;
+    var tmpNbr;
+    while(true){
+        tmpNbr = mostRepetedCharacter+ otherCount.toString()
+        if (str.indexOf(tmpNbr) < 0){
+            return [tmpNbr-10, tmpNbr-1]
+            break;
+        }
+        otherCount++
+        if (otherCount === 10) {
+            mostRepetedCharacter = (mostRepetedCharacter.parseInt()+1).toString()
+            otherCount = 0
+        }
+    }
+}
