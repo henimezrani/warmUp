0719446853b3bee7a82f9837a4e7f5edadd7464a
henimezrani <henimezrani@gmail.com>
2019-11-05 09:35:36 +0100

    warmUp 15 solved

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,34 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+
+function invertStr(str) {
+	var tmpArr = [];
+	var spaceIndx = 0;
+	var countWords = 0;
+	var result = "";
+
+	for (var i = 0; i < str.length; i++) {
+		if ((str[i] === " ") || (i === str.length-1)){
+			var tmpStr = ""
+			for ( var j = spaceIndx ; j < i ; j++){
+				tmpStr += str[j]
+			}
+			if (i === str.length-1) {
+				tmpStr += str[i]
+			}
+			tmpArr[countWords] = tmpStr
+			spaceIndx = i + 1
+			countWords++;
+		}
+	}
+	for (var i = tmpArr.length-1 ; i >=0 ; i--){
+		result += tmpArr[i]
+		if (i !== 0) {
+			result += " "
+		}
+	}
+	return result;
+
+}
