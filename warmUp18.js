0781fc21eadbb8dcdb33c60230c828b4bc9bbcbe
henimezrani <henimezrani@gmail.com>
2019-11-09 09:24:09 +0100

    last warmUp solved

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,18 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+function countChars(string) {
+	var tmpobj = {}
+	var result = ""
+	for (var i = 0; i < string.length; i++) {
+		if (tmpobj.hasOwnProperty(string[i])){
+			tmpobj[string[i]] +=1;
+			result+= tmpobj[string[i]];
+		} else {
+			tmpobj[string[i]] =1;
+			result+= tmpobj[string[i]];
+		}
+	}
+	return result;
+}
